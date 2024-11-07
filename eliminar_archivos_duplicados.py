03a5ded2501efe5eda7348d6ec2a844ebc15433d
Frodinsky <rodolara97@gmail.com>
2024-11-07 09:46:00 -0700

    Eliminanado posibles errores

--- eliminar_archivos_duplicados.py
+++ eliminar_archivos_duplicados.py
@@ -2,14 +2,12 @@ import os
 import hashlib
 
 def hash_file(filename):
-    try:
-        h = hashlib.md5()
-        with open(filename, "rb") as file:
-            while chunk := file.read(8192): #para que los archivos se dividan en 8kb aprox
-                h.update(chunk)
-        return h.hexdigest()
-    except Exception as e:
-        return None
+    h = hashlib.md5()
+    with open(filename, "rb") as file:
+        while chunk := file.read(8192): #para que los archivos se dividan en 8kb aprox
+            h.update(chunk)
+    return h.hexdigest()
+
 
 def find_duplicates(folder):
     hashes = {}
@@ -18,11 +16,10 @@ def find_duplicates(folder):
         for f in filenames:
             full_path = os.path.join(dirpath, f )
             file_hash = hash_file(full_path)
-            if file_hash is not None:
-                if file_hash in hashes:
-                    duplicates.append((full_path, hashes[file_hash]))
-                else:
-                    hashes[file_hash] = full_path
+            if file_hash in hashes:
+                duplicates.append((full_path, hashes[file_hash]))
+            else:
+                hashes[file_hash] = full_path
     return  duplicates
 
 def delete_file(filepath):
